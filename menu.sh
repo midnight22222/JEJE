e31faba3ff9620901ee9bb712e30d900e427f4e6
midnight22222 <33739517+midnight22222@users.noreply.github.com>
2017-12-22 23:12:51 +0700

    Add files via upload

---
+++ menu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Menu
+
+echo -e "* menu      : เรียก เมนู"
+echo -e "* usernew   : สร้าง User ใหม่ "
+echo -e "* trial     : สร้าง User TEST"
+echo -e "* hapus     : menghapus akun SSH & OpenVPN"
+echo -e "* cek       : cek user login"
+echo -e "* member    : daftar member SSH & OpenVPN"
+echo -e "* resvis    : restart service dropbear, webmin"
+echo -e "              squid3, OpenVPN dan SSH"
+echo -e "* reboot    : reboot VPS"
+echo -e "* speedtest : speedtest VPS"
+echo -e "* info      : menampilkan informasi sistem"
+echo -e "* about     : info script auto install"
+echo -e "* exit      : keluar dari Putty/Connecbot/"
+echo -e "              JuiceSSH"
+echo -e ""
